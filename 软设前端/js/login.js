68fda03ab91215628279a4590a933470385edd4d
hailian.lei <2522292854@qq.com>
2021-10-31 13:40:53 +0800

    登录数据交互

--- 软设前端/js/login.js
+++ 软设前端/js/login.js
@@ -3,56 +3,56 @@ window.addEventListener('load', function() {
     var phone = login.querySelector('.phoneNo'); //获取账号输入框
     var psd = login.querySelector('.password'); //获取密码输入框
     var code = login.querySelector('.code'); //获取验证码输入框
+    var vcode = login.querySelector('.vcode'); //获取验证码图片
     var loginBtn = login.querySelector('button'); //获取登录按钮
     var curBox = login.querySelector('.curBox');
-    phone.addEventListener('blur', function() {
-            var results = phoneFun(phone.value);
-            if (results == false) {
-                curBox.classList.add('cur');
-            } else {
-                curBox.classList.remove('cur');
-            }
-        })
-        // 判断手机号码输入是否正确
-    function phoneFun(phone) {
-        var myreg = /^[1][3,4,5,7,8,9][0-9]{9}$/;
-        if (!myreg.test(phone)) {
+
+    loginBtn.addEventListener('click', function(e) {
+        if (phone.innerHTML == "" || psd.innerHTML == "" || code == "") {
+            alert("请输入用户名");
             return false;
-        } else {
-            return true;
         }
-    }
-    // 判断密码输入正不正确
-    // psd.addEventListener('blur', function() {
-
-    //     })
-
-    loginBtn.addEventListener('click', function() {
-
+        phone.addEventListener('blur', function() {
+                var results = phoneFun(phone.value);
+                if (results == false) {
+                    curBox.classList.add('cur');
+                } else {
+                    curBox.classList.remove('cur');
+                }
+            })
+            // 判断手机号码输入是否正确
+        function phoneFun(phone) {
+            var myreg = /^[1][3,4,5,7,8,9][0-9]{9}$/;
+            if (!myreg.test(phone)) {
+                return false;
+            } else {
+                return true;
+            }
+        }
         $.ajax({
             type: 'post',
             url: 'http://rsrs.nat300.top/rs/UserSignServlet',
-            dataType: "json",
-            processData: false,
-            contentType: false,
+            dataType: 'json',
+
             data: {
-                'action': login,
-                'phone': phone,
-                'pwd': psd
+                'action': "login",
+                'phone': JSON.stringify(phone),
+                'pwd': JSON.stringify(psd),
+                'vcode': JSON.stringify(code)
             },
             success: function(result) {
-                console.log(result);
+                if (result.msg == '用户不存在') {
+                    alert("用户名错误或密码错误");
+                }
+                if (result.code == false) {
+                    alert("验证码错误");
+                    code.innerHTML = "";
+                }
+
+            },
+            error: function(aa) {
+                console.log(aa);
             }
         })
-        var datas = JSON.parse(success());
-    })
-
-    // 判断验证码输入正不正确
-    code.addEventListener('blur', function() {
-        if (datas.code == false) {
-            curBox.classList.add('cur');
-        } else {
-            curBox.classList.remove('cur');
-        }
     })
 })
